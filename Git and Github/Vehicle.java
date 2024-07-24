4b5fdc637a3c0004101555c588d8965cde9686f4
harshitasatija8@gmail.com <harshitasatija8@gmail.com>
2024-07-25 01:02:14 +0530

    New branch

---
+++ Git and Github/Vehicle.java
@@ -0,0 +1,21 @@
+class Vehicle
+{
+  public static void main(String[] args)
+{
+int x;
+{
+x=70;
+int y=80;
+ System.out.println(x);
+ System.out.println(y);
+x=93;
+}
+ System.out.println(x);
+{
+ System.out.println(x);
+x=60;
+ }
+System.out.println(x);
+}
+
+
